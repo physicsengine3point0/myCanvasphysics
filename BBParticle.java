18fc60317eee8aa02bb4e291d38b619d82a2506c
physicsengine3point0 <nabaskes@gmail.com>
2013-05-22 14:14:01 -0300

    Created Basic Blue Particle class
    Created Basic Blue Particle class, a default particle for testing, etc.

---
+++ BBParticle.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+
+
+public class BBParticle extends Particle
+{
+  public BBParticle(double xloc, double yloc, double direct, double spe)
+	{
+		super(xloc, yloc, direct, spe, Color.blue, 10, 1);
+	}
+
+}
