29b97efc76318d03f855fbf83704a92b47f2f47a
physicsengine3point0 <nabaskes@gmail.com>
2013-05-23 14:08:04 -0300

    updated BBParticle

--- BBParticle.java
+++ BBParticle.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 
 public class BBParticle extends Particle
 {
-  public BBParticle(double xloc, double yloc, double direct, double spe)
+	public BBParticle(double xloc, double yloc, double direct, double spe)
 	{
 		super(xloc, yloc, direct, spe, Color.blue, 10, 1);
 	}
